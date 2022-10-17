8203452fed1f7076b77c88fa3f97fb6fbde11030
László Nagy <laszlo.nagy4@stud.uni-corvinus.hu>
2022-10-17 13:42:20 +0200

    GetExchangeRates

--- gyak05/Form1.cs
+++ gyak05/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak05.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,27 @@ namespace gyak05
 {
     public partial class Form1 : Form
     {
+        
+        
         public Form1()
         {
             InitializeComponent();
+            GetExchangeRates();
+        }
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames="EUR",
+                startDate="2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+            richTextBox1.Text=result;
         }
     }
 }
