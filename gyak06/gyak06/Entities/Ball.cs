e5020c2f9e7085f5c1978205b3de282dc93fcdc5
László Nagy <laszlo.nagy4@stud.uni-corvinus.hu>
2022-11-03 20:57:42 +0100

    new project

--- gyak06/gyak06/Entities/Ball.cs
+++ gyak06/gyak06/Entities/Ball.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace gyak06
 {
-    internal class Ball
+    internal class Ball:Label
     {
+
     }
 }
