8f07a7cbee45c46d3b422d897b1a2ba9d060ca7c
László Nagy <laszlo.nagy4@stud.uni-corvinus.hu>
2022-11-03 20:47:31 +0100

    new class

---
+++ gyak06/gyak06/Ball.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak06
+{
+    internal class Ball
+    {
+    }
+}
