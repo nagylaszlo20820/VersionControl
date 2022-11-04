6fc8a39cb41616254ebc681dbfca6888186c99d1
László Nagy <laszlo.nagy4@stud.uni-corvinus.hu>
2022-11-04 10:54:57 +0100

    ballfactory,carfactory

--- gyak06/gyak06/Entities/CarFactory.cs
+++ gyak06/gyak06/Entities/CarFactory.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace gyak06
 {
-    public class BallFactory:IToyFactory
+    public class CarFactory:IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
