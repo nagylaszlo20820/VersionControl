9827b2f2a8fa91aeef7b76e423baee90c6742bc0
László Nagy <laszlo.nagy4@stud.uni-corvinus.hu>
2022-11-04 10:41:57 +0100

    ball from toy

--- gyak06/gyak06/Abstractions/Toy.cs
+++ gyak06/gyak06/Abstractions/Toy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,20 +15,21 @@ namespace gyak06
             AutoSize = false;
             Width = 50;
             Height = Width;
-            Paint += Ball_Paint;
+            
+            Paint += Toy_Paint;
         }
 
-        private void Ball_Paint(object sender, PaintEventArgs e)
+        private void Toy_Paint(object sender, PaintEventArgs e)
         {
             DrawImage(e.Graphics);
         }
 
-        protected void DrawImage(Graphics g)
-        {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
 
-        public void MoveBall()
+
+        protected abstract void DrawImage(Graphics g);
+        
+
+        public void MoveToy()
         {
             Left += 1;
         }
