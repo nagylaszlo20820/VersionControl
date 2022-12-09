6623ff39fd5170b6e1c5f30829dc7a51a970ba5f
László Nagy <laszlo.nagy4@stud.uni-corvinus.hu>
2022-12-09 17:07:27 +0100

    ..

--- Evolúciós algoritmus/Evolúciós algoritmus/Form1.cs
+++ Evolúciós algoritmus/Evolúciós algoritmus/Form1.cs
@@ -63,21 +63,7 @@ namespace Evolúciós_algoritmus
             }
             
 
-            gc.ResetCurrentLevel();
-            foreach (var p in topPerformers)
-            {
-                var b = p.Brain.Clone();
-                if (generation % 3 == 0)
-                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
-                else
-                    gc.AddPlayer(b);
-
-                if (generation % 3 == 0)
-                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
-                else
-                    gc.AddPlayer(b.Mutate());
-            }
-            gc.Start();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
